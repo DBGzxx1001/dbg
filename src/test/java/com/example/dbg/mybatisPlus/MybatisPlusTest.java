7d66cfb48738defbc041bcaad205bdd18a00e1d9
guxinglei1001 <751936320@qq.com>
2018-10-17 10:05:06 +0800

    springboot+mybatis 框架简单搭建

---
+++ src/test/java/com/example/dbg/mybatisPlus/MybatisPlusTest.java
@@ -0,0 +1,31 @@
+package com.example.dbg.mybatisPlus;
+
+import com.example.dbg.mapper.UserMapper;
+import com.example.dbg.model.po.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @user DBG_ZXX
+ * @date 2018/10/16 14:24
+ * @desc mybatisPlus 测试
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPlusTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void selectAllTest(){
+        List<User> list = userMapper.selectList(null);
+        list.forEach(System.out::println);
+    }
+
+}
